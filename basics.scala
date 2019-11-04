48ac38712cc7456268cecc6cff433ebd79cfd24e
Girishsanthosh Ranganathan <40407062+girish010789@users.noreply.github.com>
2019-11-03 21:21:14 -0800

    Update basics.scala

--- basics.scala
+++ basics.scala
@@ -1,10 +1,10 @@
-##variable and constants declaration
+//variable and constants declaration
 var name = "girish" (variable)
 val name = 34  (constant)
 
 Variables are mutable whereas constants are not mutable
 
-##if else is similar to C/Java
+//if else is similar to C/Java
 
 if (name == "girish") {
   println ("name is correct")
@@ -12,27 +12,27 @@ if (name == "girish") {
   println ("name is incorrect")
 }
 
-##It has list data type which is declared using List method in the library
+//It has list data type which is declared using List method in the library
 var list1 = List(1,2,3)
 
-##methods/functions in scala declared using keyword "def". While passing arguments to functions, we need to specify argument name, type and return type.
+//methods/functions in scala declared using keyword "def". While passing arguments to functions, we need to specify argument name, type and return type.
 def distance(x: Int, y: Int, returnPositive: Boolean): Int = {
     val xy = x * y
     if (returnPositive) xy.abs else -xy.abs
 }
 
-##parameters of the method are passed along with parameter name (i.e in the example below, the parameter name count,wrap and wrapValue are specified during passing the value to the function)
+//parameters of the method are passed along with parameter name (i.e in the example below, the parameter name count,wrap and wrapValue are specified during passing the value to the function)
 
 def myMethod(count: Int, wrap: Boolean, wrapValue: Int = 24): Unit = { ... }
 myMethod(count = 10, wrap = false, wrapValue = 23)
 
 
 
-##functions can be overloaded (i.e same function name with different functionalities), but overloading is not advisable
+//functions can be overloaded (i.e same function name with different functionalities), but overloading is not advisable
 def times2(x: Int): Int = 2 * x
 def times2(x: String): Int = 2 * x.toInt
 
-##Recursive and nested functions
+//Recursive and nested functions
 def asciiTriangle(rows: Int) {
     
     // This is cute: multiplying "X" makes a string with many copies of "X"
@@ -47,20 +47,20 @@ def asciiTriangle(rows: Int) {
 // printRow(1) // This would not work, since we're calling printRow outside its scope
 asciiTriangle(6)
 
-##for statement is similar to many other language, where you can iterate the range of values
+//for statement is similar to many other language, where you can iterate the range of values
 for (i <- 0 to 10) { println(i) }
 
-##use 'until to omit the final value
+//use 'until to omit the final value
 for (i <- 0 to 10) { println(i) }   ; this will print from only 0 to 9
 
-##packages and classes are imported using "import keyword". 
+//packages and classes are imported using "import keyword". 
 import tool1.tool2 ; this means tool2 is a class inside the package tool1
 
-####Scala is an object oriented language like C++ and java and Classes are an important part of OOP. 
-##Class is declared using keyword class and object is created to specify the instance of the class.
-##Class is an abstract which contains variables and methods.
-##Class can extend other classes (i.e have a super class and subclass) and subclass can selectively (or completely) inherit the properties of superclass.
-##Class can have parameters passed directly to it instead of passing through methods/constructor.
+//Scala is an functional language and not object oriented like C++ and java. Classes are still important in scala.
+//Class is declared using keyword class and object is created to specify the instance of the class.
+//Class is an abstract which contains variables and methods.
+//Class can extend other classes (i.e have a super class and subclass) and subclass can selectively (or completely) inherit the properties of superclass.
+//Class can have parameters passed directly to it instead of passing through methods/constructor.
 
 class WrapCounter(counterBits: Int) {
 
@@ -77,5 +77,5 @@ class WrapCounter(counterBits: Int) {
   println(s"counter created with max value $max")
 }
 
-##instance of the class is created using new keyword
+//instance of the class is created using new keyword
 val x = new WrapCounter(2)
